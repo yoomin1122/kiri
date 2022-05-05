dd2ed384383697096fa4350245c776e86dc3b147
Yoo Ho <79196965+yoomin1122@users.noreply.github.com>
2022-05-05 19:58:10 +0900

    Update Core.py

--- Cog/Core.py
+++ Cog/Core.py
@@ -25,10 +25,10 @@ from discord_slash.context import MenuContext
 from discord_slash.model import ContextMenuType
 from discord_slash.model import SlashCommandOptionType
 
-
-naver_dev_id = "token"
-naver_dev_pass = "token"
-api_key = "token"
+riot_token = ""
+naver_dev_id = ""
+naver_dev_pass = ""
+api_key = ""
 
 class Core(commands.Cog):
 
@@ -210,6 +210,35 @@ class Core(commands.Cog):
         embed1.set_image(url=url)
         await ctx.reply(embed=embed1, mention_author=False)
 
+    @commands.command()
+    async def 롤(self, ctx, *, UserName):
+            UserInfoUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + UserName
+            requests = requests.get(UserInfoUrl, headers={"X-Riot-Token":riot_token})
+            requests_js = json.loads(requests.text)
+
+            if requests.status_code == 200:
+                UserIconUrl = "http://ddragon.leagueoflegends.com/cdn/11.3.1/img/profileicon/{}.png"
+                embed = discord.Embed(title=f"{requests_js['name']} 님의 플레이어 정보", description=f"**{requests_js['summonerLevel']} LEVEL**", color=0x6758f0)
+
+                UserInfoUrl_2 = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + requests_js["id"]
+                res_2 = requests.get(UserInfoUrl_2, headers={"X-Riot-Token":riot_token})
+                res_2js = json.loads(res_2.text)
+
+                if res_2js == []:
+                    embed.add_field(name=f"{requests_js['name']} 님은 언랭크입니다.", value="**언랭크 유저의 정보는 출력하지 않습니다.**", inline=False)
+                else:
+                    for rank in res_2js:
+                        if rank["queueType"] == "RANKED_SOLO_5x5":
+                            embed.add_field(name="솔로랭크", value=f"**티어 : {rank['tier']} {rank['rank']} - {rank['leaguePoints']} LP**\n"f"**승 / 패 : {rank['wins']} 승 {rank['losses']} 패**", inline=True)
+                        else:
+                            embed.add_field(name="자유랭크", value=f"**티어 : {rank['tier']} {rank['rank']} - {rank['leaguePoints']} LP**\n"f"**승 / 패 : {rank['wins']} 승 {rank['losses']} 패**", inline=True)
+                embed.set_author(name=requests_js['name'], url=f"http://fow.kr/find/{UserName.replace(' ', '')}", icon_url=UserIconUrl.format(requests_js['profileIconId']))
+                await ctx.send(embed=embed)
+
+            else:
+                error = discord.Embed(title="존재하지 않는 소환사명입니다.\n다시 한번 확인해주세요.", color=0xFF9900)
+                await ctx.send(embed=error)
+
     @commands.command(aliases=['프사'])
     @commands.cooldown(1, 30, commands.BucketType.user)
     async def 아바타(self, ctx, user: discord.User):
@@ -222,6 +251,6 @@ class Core(commands.Cog):
     async def 아바타_error(self, ctx: commands.Context, error):
         if isinstance(error, commands.CommandOnCooldown):
             await ctx.reply(f"쿨타임이 {round(error.retry_after)}초 남았습니다")
-
+# 0xfa4f4f, 0x00FF7
 def setup(bot):
     bot.add_cog(Core(bot))
