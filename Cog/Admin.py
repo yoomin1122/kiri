486f0e564a36db129d684ead8a99a4e24884322f
Yoo Ho <79196965+yoomin1122@users.noreply.github.com>
2022-05-21 23:53:27 +0900

    Update Admin.py

--- Cog/Admin.py
+++ Cog/Admin.py
@@ -19,7 +19,7 @@ class Admin(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    @commands.has_permissions(kick_members=True)
+    @commands.has_permissions(administrator=True)
     async def 청소(self, ctx, number: int = None):
         if ctx.guild:
             if ctx.message.author.guild_permissions.manage_messages:
@@ -40,56 +40,51 @@ class Admin(commands.Cog):
                 await ctx.reply('이 명령을 사용할 수 있는 권한이 없습니다.')
         else:
             await ctx.reply('DM에선 불가합니다.')
-    @commands.has_permissions(kick_members=True)
-    @cog_ext.cog_slash(name="번역", description="원하는 문장을 번역 해보세요!", 
+
+    @commands.has_permissions(administrator=True)
+    @cog_ext.cog_slash(name="청소", description="청소를 할수 있습니다", 
     options=[create_option(name="number", description="삭제를 원하는 메시지를 1부터 99까지의 숫자만큼 적어주세요", required=True, option_type=4)])
     async def clean(self, ctx, number: int = None):
-        if ctx.guild:
-            if ctx.message.author.guild_permissions.manage_messages:
-                try:
+        if ctx.guild:   
                     if number is None:
                         await ctx.reply('숫자를 입력해주세요.')
                     elif 100 < number:
                         await ctx.message.delete()
-                        await ctx.send(f'{ctx.message.author.mention} 100보다 큰 수는 입력할 수 없습니다.', delete_after=5)
+                        await ctx.send(f'{ctx.author.mention} 100보다 큰 수는 입력할 수 없습니다.', delete_after=5)
                     else:
-                        deleted = await ctx.message.channel.purge(limit=number)
+                        deleted = await ctx.channel.purge(limit=number)
                         embed = discord.Embed(title="청소 완료",description=f"청소된 메세지 : {len(deleted)}개",  color = 0xfa4f4f)
-                        embed.set_footer(text=f"청소한 관리자 : {ctx.message.author.name}")
+                        embed.set_footer(text=f"청소한 관리자 : {self.bot.author.name}")
                         await ctx.send(embed=embed)
-                except:
-                    await ctx.reply("삭제가 불가합니다.")
-            else:
-                await ctx.reply('이 명령을 사용할 수 있는 권한이 없습니다.')
-        else:
+        else:   
             await ctx.reply('DM에선 불가합니다.')
 
-    @commands.has_permissions(kick_members=True)
+    @commands.has_permissions(administrator=True)
     @cog_ext.cog_slash(name="킥", description="원하는 유저를 내보냅니다 ", 
     options=[
         create_option(name="member", description="킥을 원하는 유저를 선택해주세요", required=True, option_type=6),
-        create_option(name="reason", description="삭제를 원하는 메시지를 1부터 99까지의 숫자만큼 적어주세요", required=True, option_type=3)])
-    async def clean(self, ctx, member = discord.Member, reason:str = None):
+        create_option(name="reason", description="사유를 적어주세요", required=True, option_type=3)])
+    async def kick(self, ctx, member = discord.Member, reason:str = None):
         if ctx.guild:
-            if ctx.message.author.guild_permissions.manage_messages:
                 await member.kick(reason=reason)
                 await ctx.send(f'유저 {member}을(를) 내보냈습니다. \n사유 : {reason}')
-            else:
-                await ctx.reply('이 명령을 사용할 수 있는 권한이 없습니다.')
         else:
             await ctx.reply('DM에선 불가합니다.')
 
     @commands.command(aliases=["kick"])
-    @commands.has_permissions(kick_members=True)
+    @commands.has_permissions(administrator=True)
     async def 킥(self, ctx, member: discord.Member, *, reason=None):
         await member.kick(reason=reason)
         await ctx.send(f'유저 {member}을(를) 내보냈습니다. \n사유 : {reason}')
 
     @commands.command(aliases=["ban", "차단"])
-    @commands.has_permissions(kick_members=True)
+    @commands.has_permissions(administrator=True)
     async def 밴(self, ctx, member: discord.Member, *, reason=None):
-        await member.ban(reason=reason)
-        await ctx.send(f'유저 {member}을(를) 차단하였습니다.')
+        if ctx.guild:
+                await member.ban(reason=reason)
+                await ctx.send(f'유저 {member}을(를) 밴하였습니다. \n사유 : {reason}')
+        else:
+            await ctx.reply('DM에선 불가합니다.')
 
     @commands.command()
     async def 뮤트(self, ctx, member: discord.Member, minutes: int):
@@ -100,7 +95,7 @@ class Admin(commands.Cog):
         await member.timeout(until)
 
     @commands.command(aliases=['해제'])
-    @commands.has_permissions(kick_members=True)
+    @commands.has_permissions(administrator=True)
     async def 언밴(self, ctx, nickname: str):
         ban_entry = await ctx.guild.bans()
         for users in ban_entry:
